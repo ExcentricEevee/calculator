bec197b7e04af5ff3cecdb1dda2a8d28097794db
Curtis <excentriceevee@hotmail.com>
2023-05-03 03:36:57 -0400

    Create function for determining which operation to use

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let num1, operator, num2;
 
+function operate(x, y, operator) {
+    switch(operator) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+    }
+}
+
 function add(x, y) {
     return x + y;
 }
