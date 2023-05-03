577fcfee1aa685cc0c4cde014d38a267910d05d0
Curtis <excentriceevee@hotmail.com>
2023-05-03 03:33:29 -0400

    Create arithmatic functions

---
+++ script.js
@@ -0,0 +1,17 @@
+let num1, operator, num2;
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
